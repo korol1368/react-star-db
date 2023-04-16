6ded0f0210e3d132e3d5e8db103ba11ef0b00342
alex <alex@MacBook-Air-Mac.local>
2023-04-16 16:35:48 +0300

    Use componentDidMount

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -43,7 +43,7 @@ export default class SwapiService {
     return item.url.match(idRegExp)[1];
   }
 
-  _transformPlanet(planet) {
+  _transformPlanet = (planet) => {
     return {
       id: this._extractId(planet),
       name: planet.name,
@@ -51,9 +51,9 @@ export default class SwapiService {
       rotationPeriod: planet.rotation_period,
       diameter: planet.diameter,
     };
-  }
+  };
 
-  _transformStarship(starship) {
+  _transformStarship = (starship) => {
     return {
       id: this._extractId(starship),
       name: starship.name,
@@ -65,9 +65,9 @@ export default class SwapiService {
       passengers: starship.passengers,
       cargoCapacity: starship.cargoCapacity,
     };
-  }
+  };
 
-  _transformPerson(person) {
+  _transformPerson = (person) => {
     return {
       id: this._extractId(person),
       name: person.name,
@@ -75,5 +75,5 @@ export default class SwapiService {
       birthYear: person.birthYear,
       eyeColor: person.eyeColor,
     };
-  }
+  };
 }
